9d2c14638769d91601161b85c7afa99cd481d576
Marh ziad <marhziad4@gmail.com>
2022-01-16 22:08:43 +0200

    SlidingUpPanel

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:trends_app/data/dummy_data.dart';
 import 'package:trends_app/ui/second_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:trends_app/widget/authors_widget.dart';
+import 'package:trends_app/widget/post_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -13,12 +16,12 @@ class HomeScreen extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      localeResolutionCallback: (currentLang,supportLang){
-        if(currentLang != null){
-          for(Locale locale in supportLang){
-              if(locale.languageCode == currentLang.languageCode){
-                return currentLang;
-              }
+      localeResolutionCallback: (currentLang, supportLang) {
+        if (currentLang != null) {
+          for (Locale locale in supportLang) {
+            if (locale.languageCode == currentLang.languageCode) {
+              return currentLang;
+            }
           }
           return supportLang.first;
         }
@@ -33,256 +36,199 @@ class HomeScreen extends StatelessWidget {
         ),
         drawer: Drawer(),
 
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/greenbackground2.jpg'),
-                fit: BoxFit.cover),
-          ),
-          child: Container(
-            margin: EdgeInsets.only(top: 130),
-            child: Column(
-              children: [
-                Text(
-                  "Browse",
-                  style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "find prodcast that suit to your interest",
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 20,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: BorderSide(
-                          color: Color(0XFFF3F3F3),
-                        ),
-                      ),
-                      hintText: "    type keyboard",
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      fillColor: Colors.white54,
-                      filled: true,
+        body: SlidingUpPanel(
+          parallaxEnabled: true,
+          borderRadius: BorderRadiusDirectional.only(
+              topEnd: Radius.circular(40),
+              topStart: Radius.circular(40),
+            ),
+            maxHeight: 750,
+            minHeight: 300,
+            panel: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Handpicked",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      letterSpacing: 2.0,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.white,
-                              backgroundImage:  AssetImage('assets/images/star.png',
-                        ),
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              "Populer",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.white,
-                              backgroundImage: AssetImage('assets/images/increase.png'),
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              "Trending",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.white,
-                              backgroundImage:AssetImage('assets/images/history.png'),
-
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              "Recent",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.white,
-                              backgroundImage: AssetImage('assets/images/magic.png'),
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              "Magic",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 70,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return SecondScreen();
-                        },
-                      ),
-                    );
-                  },
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    height: 325,
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    width: 50,
                     decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadiusDirectional.only(
-                        topEnd: Radius.circular(40),
-                        topStart: Radius.circular(40),
+                      // borderRadius: BorderRadiusDirectional.all(Radius.circular(50)),
+                      border: Border(
+                        bottom: BorderSide(color: Colors.amber, width: 8.0),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Handpicked",
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,
-                              letterSpacing: 2.0,
-                            ),
-                          ),
-                          SizedBox(height: 20,),
-                          Row(
-                          children: [
-                            Container(
-                              width: 100,
-                              height: 90,
-                              decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadiusDirectional.all(Radius.circular(30)),
-                                image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1550226891-ef816aed4a98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),fit:BoxFit.cover),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    "Financial Freedom",
-                                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    height: 15,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 110),
-                                    child: Text(
-                                      "Natasha Rose",
-                                      style: TextStyle(
-                                        fontSize: 11,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                          ),
-                          SizedBox(height: 15,),
+                  ),
+                  Column(
+                    children: AllPost.map((e) => PostWidget(e)).toList(),
 
-                          Row(
-                          children: [
-                            Container(
-                              width: 100,
-                              height: 90,
-                              decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadiusDirectional.all(Radius.circular(30)),
-                                image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1550226891-ef816aed4a98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),fit:BoxFit.cover),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    "Financial Freedom",
-                                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    height: 15,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 110),
-                                    child: Text(
-                                      "Natasha Rose",
-                                      style: TextStyle(
-                                        fontSize: 11,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
+                  ),
+                  SizedBox(height: 100,),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Top Authors",
+                      style: TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    children: users.map((e) => AuthorsWidget(e)).toList(),
+
+                  ),
+                ],
+              ),
+
+            ),
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/greenbackground2.jpg'),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                margin: EdgeInsets.only(top: 150),
+                child: Column(
+                  children: [
+                    Text(
+                      "Browse",
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "find prodcast that suit to your interest",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 30),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 20,
                           ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide(
+                              color: Color(0XFFF3F3F3),
+                            ),
+                          ),
+                          hintText: "    type keyboard",
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          fillColor: Colors.white54,
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: AssetImage(
+                                    'assets/images/star.png',
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  "Populer",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: AssetImage(
+                                      'assets/images/increase.png'),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  "Trending",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: AssetImage(
+                                      'assets/images/history.png'),
+
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  "Recent",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: AssetImage(
+                                      'assets/images/magic.png'),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  "Magic",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
         ),
+
+
       ),
     );
   }
